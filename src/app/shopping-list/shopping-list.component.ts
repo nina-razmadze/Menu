c9f40974b6e93e5c0a113d1007b172363d979341
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-26 23:17:42 +0400

    LoopIngredients

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 10),
+    new Ingredient('Tomatoes', 6),
+  ];
+}
