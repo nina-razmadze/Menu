1f503459ad94185a5342fd1482029063772d09e4
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-10-29 19:02:44 +0400

    HostBinding

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,18 +1,18 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
-  selector: '[app-dropdown]',
+  selector: '[appDropdown]',
 })
 export class DropdownDirective {
-  private isOpen: boolean = false;
-  constructor(private Elref: ElementRef, private renderer: Renderer2) {}
+  @HostBinding('class.open') isOpen: boolean = false;
 
   @HostListener('click') onClick() {
     this.isOpen = !this.isOpen;
-    if (this.isOpen) {
-      this.renderer.addClass(this.Elref.nativeElement, 'open');
-    } else {
-      this.renderer.removeClass(this.Elref.nativeElement, 'open');
-    }
   }
 }
