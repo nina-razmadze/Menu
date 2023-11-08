5a87f0fb9a93ecce43e9d955a754cf931ce63619
nina-razmadze <123087063+nina-razmadze@users.noreply.github.com>
2023-11-08 20:29:43 +0400

    RouterShoppingList

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes' },
   { path: 'recipes', component: RecipesComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
-@NgModule({})
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
 export class AppRoutingModule {}
